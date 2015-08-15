e0fa45f57ebc88e3851c107c0e3f43570e84404f
vagrant <vagrant@localhost.localdomain>
2015-08-15 19:00:19 +0200

    cleanup

--- sms-workers/messages.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-app = Celery('messages', broker='amqp://localhost')
-
-@app.task
-def add(x, y):
-    return x + y
--- sms-workers/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-app = Celery('tasks', backend='amqp://localhost/', broker='amqp://localhost/')
-app.config_from_object('celeryconfig')
-@app.task
-def add(x, y):
-    return x + y
