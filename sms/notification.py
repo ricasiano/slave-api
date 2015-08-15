b1622769ec83de47e1f1de90ee28c012a8d781da
vagrant <vagrant@localhost.localdomain>
2015-08-15 18:57:49 +0200

    cleanup

--- sms/notification.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-app = Celery('notification', backend='amqp://localhost/', broker='amqp://localhost/')
-app.config_from_object('celeryconfig')
-
-@app.task
-def accept(data):
-    return data.get('message_type')
