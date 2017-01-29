beb248dfbfe46a06ee1628980ab30b732c0d7d35
Ubuntu <ubuntu@ip-172-31-24-131.ap-northeast-1.compute.internal>
2017-01-29 11:19:23 +0000

    modify to only accept 1 parameter

--- sms/routing/post/notification.py
+++ sms/routing/post/notification.py
@@ -10,7 +10,7 @@ app = Celery('notification', backend='amqp://localhost/', broker='amqp://localho
 app.config_from_object('configs.celery')
 
 @app.task
-def accept(data, path):
+def accept(data):
     time.sleep(3)
     subscribers.insert_one({'msisdn': '639171111111', 'subscriptions': [{'tts': {'messages':[{'message': data.get('message')}]}}]})
     return data.get('message_type')
